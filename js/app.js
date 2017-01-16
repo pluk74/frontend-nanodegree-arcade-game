f81f2c93f8b1cb7d1da7908013c600660bf0692f
Patrick Luk <Patrick Luk>
2017-01-16 14:32:57 -0500

    first commit: player movement

--- js/app.js
+++ js/app.js
@@ -24,11 +24,49 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype.update = function() {
 
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(move) {
+    switch(move) {
+        case "left":
+            console.log("left");
+            this.x = this.x - 100;
+            break;
+        case "up":
+            console.log("up");
+            this.y = this.y - 80;
+            break;
+        case "right":
+            console.log("right");
+            this.x = this.x + 100;
+            break;
+        case "down":
+            console.log("down");
+            this.y = this.y + 80;
+            break;
+    }
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var enemy1 = new Enemy();
+var enemy2 =  new Enemy();
+
+var allEnemies = [enemy1, enemy2];
+var player = new Player(200, 280);
+
 
 
 
